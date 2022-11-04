42ae283e017ed2a0b08839511a42eac54da2f256
Dennis MutumaG <46884309+dmg254@users.noreply.github.com>
2022-11-04 10:40:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function fixNav(){
         nav.classList.remove('active')
     }
 }
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.text-animate');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.text-animate .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.text-animate',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
