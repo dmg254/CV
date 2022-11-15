719974ae5559c95c7b6113a41fdf171bd4a0a32f
Dennis MutumaG <46884309+dmg254@users.noreply.github.com>
2022-11-15 12:46:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,230 +1,30 @@
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans');
+const nav= document.querySelector('.nav')
+window.addEventListener('scroll',fixNav)
 
-*{
-    box-sizing: border-box;
-    margin:0;
-    padding: 0;
+function fixNav(){
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+
+    } else {
+        nav.classList.remove('active')
+    }
 }
 
-body{
-font-family: 'open Sans' sans-serif;
-color: #222;
-padding-bottom: 30px;
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.text-animate');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-}
-
-.container-fluid{
-    max-width: 1200px;
-    margin: 0 auto;
-}
-
-.navbar{
-    position: fixed;
-    background-color:#0F052F;
-     left: 0;
-     right: 0;
-     transition: all .3s ease-in-out;
-}
-#logo {
-    display: inline-block;
-    margin: auto; /* margin: 20px was off */
-  
-    height: 60px;;
-    width: 80%; /* correct proportions to specified height */
-   
-}
-
-.navbar-toggler{
-    color: #43D9B8;
-}
-.navbar .container-fluid{ 
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px 0;
-    transition: all .3s ease-in-out;
-}
-
-.navbar ul{
-    display: flex;
-    list-style-type: none;
-    align-items: center;
-    justify-content: center;
-}
-
-.nav-link active{
-    color: #43D9B8;
-}
-
-.navbar a{
-    color: #fff;
-    text-decoration: none;
-    padding: 7px 15px;
-    transition: all .3s ease-in-out;
-
-}
-
-.navbar.active{
-    background-color: #fff;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3) ;
-}
-.navbar.active a {
-    color:black;
-}
-
-
-
-.navbar.active .container-fluid{
-    padding: 10px 0;
-
-}
-
-.navbar a.current, .nav a:hover{
-    color: #43D9B8;
-    font-weight: bold;
-}
-/*background-image: url(https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
-*/
-
-
-.head{
-   background-image: url(http://enginess.io/assets/enginess/images/insights/2015/10/web-development-e1444737604137.jpg);
-   background-repeat: no-repeat;
-   background-size: cover;
-   background-position: bottom center;
-   color:#fff ;
-   height: 100vh;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   text-align: center;
-   Position: relative;
-    z-index:-2;
-
-}
-/* (ghost elemnt to style)Using a  before pseudo selector instead of adding an html element for the overlay*/
-.head::before{
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%; 
-    height: 100%;
-   /*RGBA- red green black alpha-is the opacity*/ 
-    background-color: rgba(0, 0, 0, 0.5) ;
-    /*z-index is basically how close to you or how close to the screen an element is*/
-    z-index: -1;
-
-}
-
- .btn a:hover{
-    color: #00BFFF;
-}
-
-.head h1{
-    font-size: 46px;
-    margin: -20px 0 20px;
-}
- .head p {
-    font-size: 20px;
-    letter-spacing: 1px;
- }
-
- .content h2, .content h3{
-    font-size: 150% ;
-    margin: 20px 0;
-      
- }
- 
- .content p{
-    color:#555 ;
-    line-height: 50px;
-    letter-spacing:1.2px ;
-    text-align: center;
-    
-     
- }
-
- .Service-box{
-    text-align: center;
-    padding: 5%;
- }
-
- .dark-section{
-    background-color: #0F052F ;
- }
-
- .white-section{
-    background-color: rgba(0, 0, 0, 0.1);
- }
-
- #footer{
-    text-align: center;
-   padding: 7% 15% ;
- }
-
- .social-icon{
-    padding: 20px 10px;
-    size: 20px;
-    color: #43D9B8;
- }
-.fa-phone{
-    color: #43D9B8;
-}
-.row {
-   padding-bottom: 15px;
- }
- .Copyright{
-    color: #fff;
- }
-
- #content{
-    margin-top: 0%;
-    
-   
- }
-
- #skills{
-    padding-top: 50px;
-    padding-bottom: 50px;
- }
-
- #skills .tools h3{
-    text-align: center;
- }
- h6{
-    margin: 7% 7% 0;
- }
-  
- #Service{
-    background-color: #0F052F;
-    color: #fff;
-    margin-top: 20%;
-    
- }
-
- #Service p{
-    color: #222;
- }
-
-
-
- #collapseWidthExample .btn-primary{
-   margin: 5% 3% 0;
- }
-
-
-
- .fa-rocket, .fa-gears, .fa-cart-shopping{
-    color: #43D9B8;
- }
- .thumb-nail{
-   text-align: center;
-    width: 20%;
-    margin: 20px;
-    border-radius: 100%;
-    margin-top: 10%;
-    
-
-/*background: url(matheus_about.png);*/
- }
+anime.timeline({loop: true})
+  .add({
+    targets: '.text-animate .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.text-animate',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
